0de27a124fa23566d4dab418fae8b69a4d47e6f4
Curtis Whitley <restday7@gmail.com>
2023-03-11 04:55:03 -0500

    drew first window

--- demo.asm
+++ demo.asm
@@ -15,12 +15,25 @@
 .include "zeropage.inc"
 .include "text.inc"
 .include "borders.inc"
+.include "windows.inc"
 
 default_irq_vector: .addr 0
 
 text_array:
     .res    96
 end_text_array:
+
+window_0:
+    .byte   0   ; upper_left_row
+    .byte   0   ; upper_left_col
+    .byte   0   ; tile_color_group
+    .byte   1   ; palette_offset
+    .byte   33  ; border_style
+    .byte   0   ; fill_char
+    .byte   106 ; total_width
+    .byte   60  ; total_height
+    .byte   0   ; cur_row
+    .byte   0   ; cur_col
 
 start:
     stz     VERA_ctrl     ; no reset/DCSEL/ADDRSEL
@@ -37,43 +50,12 @@ start:
     jsr     init_global_video_regs
     jsr     init_text_tile_information
 
-    lda     #1
-    sta     ZP_PARAM_GROUP
-    lda     #1
-    sta     ZP_PARAM_PALETTE
-    lda     #end_text_array-text_array
-    sta     ZP_PARAM_STR_SIZE
     lda     #<text_array
     sta     ZP_PARAM_STR_PTR_LO
     lda     #>text_array
     sta     ZP_PARAM_STR_PTR_HI
-    lda     #30
-    sta     ZP_PARAM_ROW
-    lda     #0
-    sta     ZP_PARAM_COL
-    ldy     #0
-make_data:
-    tya
-    sta     (ZP_PARAM_STR_PTR),y
-    iny
-    cpy     #end_text_array-text_array
-    bne     make_data
-    jsr     write_array_of_char_code_indexes
-
-    lda     #1
-    sta     ZP_PARAM_GROUP
-    lda     #1
-    sta     ZP_PARAM_PALETTE
     lda     #end_text_array-text_array
     sta     ZP_PARAM_STR_SIZE
-    lda     #<text_array
-    sta     ZP_PARAM_STR_PTR_LO
-    lda     #>text_array
-    sta     ZP_PARAM_STR_PTR_HI
-    lda     #31
-    sta     ZP_PARAM_ROW
-    lda     #0
-    sta     ZP_PARAM_COL
     ldy     #0
 make_data2:
     tya
@@ -83,127 +65,12 @@ make_data2:
     iny
     cpy     #end_text_array-text_array
     bne     make_data2
-    jsr     write_array_of_char_code_values
 
-    lda     #1
-    sta     ZP_BORDER_FILL
-    lda     #21
-    sta     ZP_BORDER_ROW
-    lda     #50
-    sta     ZP_BORDER_COL
-    lda     #0
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #$11
-    sta     ZP_BORDER_STYLE
-    lda     #10
-    sta     ZP_BORDER_WIDTH
-    lda     #15
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
-
-    lda     #1
-    sta     ZP_BORDER_FILL
-    lda     #12
-    sta     ZP_BORDER_ROW
-    lda     #12
-    sta     ZP_BORDER_COL
-    lda     #1
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #21
-    sta     ZP_BORDER_STYLE
-    lda     #6
-    sta     ZP_BORDER_WIDTH
-    lda     #7
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
-
-    lda     #0
-    sta     ZP_BORDER_FILL
-    lda     #25
-    sta     ZP_BORDER_ROW
-    lda     #40
-    sta     ZP_BORDER_COL
-    lda     #0
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #$12
-    sta     ZP_BORDER_STYLE
-    lda     #6
-    sta     ZP_BORDER_WIDTH
-    lda     #7
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
-
-    lda     #0
-    sta     ZP_BORDER_FILL
-    lda     #28
-    sta     ZP_BORDER_ROW
-    lda     #57
-    sta     ZP_BORDER_COL
-    lda     #1
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #$13
-    sta     ZP_BORDER_STYLE
-    lda     #6
-    sta     ZP_BORDER_WIDTH
-    lda     #7
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
-
-    lda     #25
-    sta     ZP_BORDER_ROW
-    lda     #50
-    sta     ZP_BORDER_COL
-    lda     #0
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #$22
-    sta     ZP_BORDER_STYLE
-    lda     #6
-    sta     ZP_BORDER_WIDTH
-    lda     #7
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
-
-    lda     #25
-    sta     ZP_BORDER_ROW
-    lda     #70
-    sta     ZP_BORDER_COL
-    lda     #0
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #$33
-    sta     ZP_BORDER_STYLE
-    lda     #6
-    sta     ZP_BORDER_WIDTH
-    lda     #7
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
-
-    lda     #23
-    sta     ZP_BORDER_ROW
-    lda     #100
-    sta     ZP_BORDER_COL
-    lda     #0
-    sta     ZP_BORDER_GROUP
-    lda     #1
-    sta     ZP_BORDER_PALETTE
-    lda     #$31
-    sta     ZP_BORDER_STYLE
-    lda     #6
-    sta     ZP_BORDER_WIDTH
-    lda     #7
-    sta     ZP_BORDER_HEIGHT
-    jsr     draw_border
+    lda     #<window_0
+    sta     ZP_WINDOW_PTR_LO
+    lda     #>window_0
+    sta     ZP_WINDOW_PTR_HI
+    jsr     create_window
 
     stz     VERA_ctrl     ; no reset/DCSEL/ADDRSEL
     lda     #ENABLE_LAYER_0|ENABLE_LAYER_1|OUTPUT_MODE_VGA
